996c1752b40db28379e1aa52d9023799f33586e7
Sagar Batham <shivsagar6912@gmail.com>
2026-01-27 20:36:50 +0530

    Add Bubble_Sort and Display_Arr classes for sorting functionality

---
+++ Sorting/Display_Arr.java
@@ -0,0 +1,8 @@
+public class Display_Arr{
+    public static void displayArr(int[]arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+}
---
+++ Sorting/Selection_Sort.java
@@ -0,0 +1,5 @@
+public class Selection_Sort {
+    public static void main(String[] args) {
+        
+    }
+}
