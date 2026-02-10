58a8bd8e67e7a3ccd84fc94d3194bc948c53055e
Sagar Batham <shivsagar6912@gmail.com>
2026-02-10 20:21:04 +0530

    Add implementation for Maximum Subarray Problem

--- Array_Questions/KadaneAlgo.java
+++ Array_Questions/KadaneAlgo.java
@@ -1,5 +1,15 @@
 public class KadaneAlgo {
     public static void main(String[] args) {
-        
+        int[] arr={2,-3,5,-1,4};
+        int sum=0;
+        int maxSum=Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            sum=sum+arr[i];
+            maxSum=Math.max(sum, maxSum);
+            if(sum<0){
+                sum=0;
+            }
+        }
+        System.out.println(maxSum);
     }
 }
