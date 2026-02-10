cba7ff9eef5b01c0a688e762c451a1b26a48db33
Sagar Batham <shivsagar6912@gmail.com>
2026-02-10 23:38:43 +0530

    Na

--- LeetCode/Prob_4.java
+++ LeetCode/Prob_4.java
@@ -1,8 +1,8 @@
 // 4. Median of Two Sorted Arrays
 public class Prob_4 {
     public static void main(String[] args) {
-        int[] num1={1,2};
-        int[] num2={3,4};
+        int[] num1={1,2,3};
+        int[] num2={4,5,6};
         int n1=num1.length;
         int n2=num2.length;
         int[] ans=new int[n1+n2];
@@ -20,17 +20,27 @@ public class Prob_4 {
         }
 
             while(i<n1){
-                ans[k++]=num1[i++];
+                ans[k]=num1[i];
+                k++;
+                i++;
             }
 
             while(j<n2){
-                ans[k++]=num2[j++];
+                ans[k]=num2[j];
+                k++;
+                j++;
             }
         
-
-
-        for (int l = 0; l < ans.length; l++) {
-            System.out.println(ans[l]);
+        
+        double answer=0;
+        if(ans.length%2==0){
+            int mid=ans.length/2;
+            answer=(double)(ans[mid]+ans[mid-1])/2;
+            System.out.println(answer);
+        }else{
+            int mid=ans.length/2+1;
+            answer=mid;
+            System.out.println(answer);
         }
     }
 }
