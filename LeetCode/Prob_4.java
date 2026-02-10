0777f3bdc33c66fb924a7b7e43bbe80a241b70fa
Sagar Batham <shivsagar6912@gmail.com>
2026-02-10 22:18:07 +0530

    Fix implementation for merging two sorted arrays in Median of Two Sorted Arrays

--- LeetCode/Prob_4.java
+++ LeetCode/Prob_4.java
@@ -1,6 +1,20 @@
 // 4. Median of Two Sorted Arrays
 public class Prob_4 {
     public static void main(String[] args) {
-        
+        int[] num1={1,2};
+        int[] num2={3,4};
+        int n=num1.length+num2.length;
+        int[] ans=new int[n];
+        for (int i = 0; i < n; i++) {
+            if(num1[i]<num2[i]){
+                ans[i]=num1[i];
+            }else{
+                ans[i]=num2[i];
+            }
+        }
+
+        for (int i = 0; i < ans.length; i++) {
+            System.out.println(ans[i]);
+        }
     }
 }
