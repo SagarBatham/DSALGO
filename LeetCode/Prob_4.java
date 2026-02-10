8e814834ab21d8609036640bfc5fade10056d83e
Sagar Batham <shivsagar6912@gmail.com>
2026-02-10 22:55:49 +0530

    Refactor merging logic in Median of Two Sorted Arrays implementation

--- LeetCode/Prob_4.java
+++ LeetCode/Prob_4.java
@@ -3,18 +3,34 @@ public class Prob_4 {
     public static void main(String[] args) {
         int[] num1={1,2};
         int[] num2={3,4};
-        int n=num1.length+num2.length;
-        int[] ans=new int[n];
-        for (int i = 0; i < n; i++) {
-            if(num1[i]<num2[i]){
-                ans[i]=num1[i];
+        int n1=num1.length;
+        int n2=num2.length;
+        int[] ans=new int[n1+n2];
+        int i=0,j=0,k=0;
+        while(i<n1&&j<n2){
+            if(num1[i]<num2[j]){
+                ans[k]=num1[i];
+                k++;
+                i++;
             }else{
-                ans[i]=num2[i];
+                ans[k]=num2[j];
+                k++;
+                j++;
             }
         }
 
-        for (int i = 0; i < ans.length; i++) {
-            System.out.println(ans[i]);
+            while(i<n1){
+                ans[k++]=num1[i++];
+            }
+
+            while(j<n2){
+                ans[k++]=num2[j++];
+            }
+        
+
+
+        for (int l = 0; l < ans.length; l++) {
+            System.out.println(ans[l]);
         }
     }
 }
