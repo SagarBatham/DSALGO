d722452c31f7e741c7e9c81105082b6d91dac701
Sagar Batham <shivsagar6912@gmail.com>
2026-03-13 22:11:44 +0530

    More Questions

---
+++ LeetCode/Prob_1046.java
@@ -0,0 +1,10 @@
+// 1046. Last Stone Weight
+public class Prob_1046 {
+    public static void main(String[] args) {
+        int[] arr={2,7,4,1,8,1};
+    }
+
+    public static int stoneBeat(int[] arr) {
+        
+    }
+}
